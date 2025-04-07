4b68d90e670d7e6fcc3935c65880e41b580885f1
Richard Liang <152163506+fearlesslyh@users.noreply.github.com>
2025-04-07 19:31:49 +0800

    feat: add html page and basic project structure

---
+++ src/main/java/com/lyh/picturerepobackend/PictureRepoBackendApplication.java
@@ -0,0 +1,17 @@
+package com.lyh.picturerepobackend;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@MapperScan("com.lyh.picturerepobackend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
+public class PictureRepoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureRepoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyh/picturerepobackend/PictureRepoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyh.picturerepobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureRepoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
