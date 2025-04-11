ef7944994b6d3a966b4710455a8f4b79dd0f5fa5
Richard Liang <152163506+fearlesslyh@users.noreply.github.com>
2025-04-11 16:38:38 +0800

    feat(config): 新增JSON和MybatisPlus配置. 用户模块完成。

---
+++ src/main/java/com/lyh/picturerepobackend/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.lyh.picturerepobackend.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author <a href=https://github.com/fearlesslyh> 梁懿豪 </a>
+ * @version 1.0
+ * @date 2025/4/11 16:09
+ */
+@Configuration
+@MapperScan("com.lyh.picturerepobackend.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return mybatisPlusInterceptor;
+    }
+}
