1c1e90f703f6d309587e1cd50932b1d6460fe73a
Richard Liang <152163506+fearlesslyh@users.noreply.github.com>
2025-04-27 21:56:18 +0800

    feat(space): 新增空间分析相关枚举及VO类，实现空间资源使用、分类、标签等分析数据结构定义

---
+++ src/main/java/com/lyh/picturerepobackend/model/vo/space/analyse/SpaceCategoryResponse.java
@@ -0,0 +1,38 @@
+package com.lyh.picturerepobackend.model.vo.space.analyse;
+
+/**
+ * @author <a href=https://github.com/fearlesslyh> 梁懿豪 </a>
+ * @version 1.0
+ * @date 2025/4/27 21:51
+ */
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 空间图片分类分析响应
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SpaceCategoryResponse implements Serializable {
+    /**
+     * 图片分类
+     */
+    private String category;
+
+    /**
+     * 图片数量
+     */
+    private Long count;
+
+    /**
+     * 分类图片总大小
+     */
+    private Long totalSize;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/lyh/picturerepobackend/model/vo/space/analyse/SpaceUsageResponse.java
@@ -0,0 +1,50 @@
+package com.lyh.picturerepobackend.model.vo.space.analyse;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author <a href=https://github.com/fearlesslyh> 梁懿豪 </a>
+ * @version 1.0
+ * @date 2025/4/27 21:52
+ */
+
+/**
+ * 空间资源使用分析响应类
+ */
+
+@Data
+public class SpaceUsageResponse implements Serializable {
+    /**
+     * 已使用大小
+     */
+    private Long usedSize;
+
+    /**
+     * 总大小
+     */
+    private Long maxSize;
+
+    /**
+     * 空间使用比例
+     */
+    private Double sizeUsageRatio;
+
+    /**
+     * 当前图片数量
+     */
+    private Long usedCount;
+
+    /**
+     * 最大图片数量
+     */
+    private Long maxCount;
+
+    /**
+     * 图片数量占比
+     */
+    private Double countUsageRatio;
+
+    private static final long serialVersionUID = 1L;
+}
