50acf2e2656cfffec8ee4a0f2db534dce969c1d6
Richard Liang <152163506+fearlesslyh@users.noreply.github.com>
2025-04-27 22:16:09 +0800

    feat(space): 实现空间验证与填充逻辑，新增空间名称长度校验及层级合法性检查，完善限额自动填充功能

--- src/main/java/com/lyh/picturerepobackend/service/impl/SpaceServiceImpl.java
+++ src/main/java/com/lyh/picturerepobackend/service/impl/SpaceServiceImpl.java
@@ -1,7 +1,12 @@
 package com.lyh.picturerepobackend.service.impl;
 
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lyh.picturerepobackend.exception.BusinessException;
+import com.lyh.picturerepobackend.exception.ErrorCode;
+import com.lyh.picturerepobackend.exception.ThrowUtils;
 import com.lyh.picturerepobackend.model.entity.Space;
+import com.lyh.picturerepobackend.model.enums.SpaceLevelEnum;
 import com.lyh.picturerepobackend.service.SpaceService;
 import com.lyh.picturerepobackend.mapper.SpaceMapper;
 import org.springframework.stereotype.Service;
@@ -16,11 +21,42 @@ public class SpaceServiceImpl extends ServiceImpl<SpaceMapper, Space> implements
 
     @Override
     public void validSpace(Space space, boolean add) {
-
+        ThrowUtils.throwIf(space == null, ErrorCode.PARAMS_ERROR, "空间不能为空");
+        //从对象取值
+        String spaceName = space.getSpaceName();
+        Integer spaceLevel = space.getSpaceLevel();
+        SpaceLevelEnum spaceLevelEnum = SpaceLevelEnum.getEnumByValue(spaceLevel);
+        if (add) {
+            if (StrUtil.isBlank(spaceName)) {
+                throw new BusinessException(ErrorCode.PARAMS_ERROR, "空间名称不能为空");
+            }
+            if (spaceLevel == null) {
+                throw new BusinessException(ErrorCode.PARAMS_ERROR, "空间级别不能为空");
+            }
+        }
+        // 修改数据时，如果要改空间级别
+        if (spaceLevel != null && spaceLevelEnum == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "空间级别不存在");
+        }
+        if (StrUtil.isNotBlank(spaceName) && spaceName.length() > 30) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "空间名称过长");
+        }
     }
 
     @Override
     public void fillSpaceBySpaceLevel(Space space) {
+        // 根据空间级别，自动填充限额
+        SpaceLevelEnum spaceLevelEnum = SpaceLevelEnum.getEnumByValue(space.getSpaceLevel());
+        if (spaceLevelEnum != null) {
+            long maxSize = spaceLevelEnum.getMaxSize();
+            if (space.getMaxSize()==null) {
+                space.setMaxSize(maxSize);
+            }
+            long maxCount = spaceLevelEnum.getMaxCount();
+            if (space.getMaxCount()==null) {
+                space.setMaxCount(maxCount);
+            }
+        }
 
     }
 }
